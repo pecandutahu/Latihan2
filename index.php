6a79deac3857e8e77e62ed65f5ca7f4cca8978c4
Herdiana <Herdiana@Herdiana-PC>
2014-12-23 22:22:05 +0700

    1.7

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         echo 'Latihan sob';
-        echo 'hoyong emam keyeup baseuh'
+        echo 'hyong nu baseuh atuh'
         ?>
     </body>
 </html>
